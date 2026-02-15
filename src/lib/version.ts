47cee1276b6dd283d527449fe5c76e3eb2abf78e
Alciney Nunes <alcineynunes790@gmail.com>
2026-02-15 12:56:38 -0300

    chore: bump version para deploy

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,3 +1,3 @@
 // Incrementado automaticamente via npm run bump-version
-export const APP_VERSION = '1.0.4'
-export const BUILD_TIMESTAMP = 1771167708241
+export const APP_VERSION = '1.0.5'
+export const BUILD_TIMESTAMP = 1771170998621
